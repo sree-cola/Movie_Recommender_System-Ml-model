c749a5e155895319e31ffaefeda2343119f5d1bc
sree-cola <84244458+sree-cola@users.noreply.github.com>
2023-01-05 22:07:30 +0530

    a

---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import bz2
+movie_dict=pickle.load(open('movie_dict.pkl','rb'))
+similarity=bz2.BZ2File("similarity1.pkl",'rb')
+newdata=pickle.load(similarity)
+movies=pd.DataFrame(movie_dict)
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = newdata[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies =[]
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+
+st.title('Movie Recommender System')
+Selected_movie_name = st.selectbox(
+'How would you like to be contacted?',
+movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations=recommend(Selected_movie_name)
+    for i in recommendations:
+        st.write(i)
